2b3124873edb4344b06228e2703e669b458d75d8
scottbasgaard <sbasgaard@gmail.com>
2014-03-21 19:21:57 +0100

    Missing text domain for widget title

--- includes/widgets/widget-most-liked.php
+++ includes/widgets/widget-most-liked.php
@@ -120,7 +120,7 @@ class P2_Likes_Widget_Most_Liked extends WP_Widget {
 		}
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'p2-likes' ); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
 		</p>
 		<?php 
